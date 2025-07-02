c0c65f448c9ad7bdb432e6cd125153dd50bc66fe
brandyodhiambo <baodhiambo@co-opbank.co.ke>
2025-07-02 08:32:43 +0300

    Initial Commit

---
+++ Majira/MajiraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MajiraApp.swift
+//  Majira
+//
+//  Created by MAC on 02/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MajiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MajiraTests/MajiraTests.swift
@@ -0,0 +1,17 @@
+//
+//  MajiraTests.swift
+//  MajiraTests
+//
+//  Created by MAC on 02/07/2025.
+//
+
+import Testing
+@testable import Majira
+
+struct MajiraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
